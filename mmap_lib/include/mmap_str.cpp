b16dea96431711276567f212eab75a481e09d675
Sloan Liu <slyliu@ucsc.edu>
2021-04-29 13:42:30 -0700

    trying to use the custom data structures but dont work

--- mmap_lib/include/mmap_str.cpp
+++ mmap_lib/include/mmap_str.cpp
@@ -4,3 +4,5 @@
 
 mmap_lib::map<std::string_view, uint32_t> mmap_lib::str::string_map2;
 //mmap_lib::map<std::string_view, uint32_t> mmap_lib::str::string_map2("lgdb", "global_str_map");
+
+//mmap_lib::vector<int> mmap_lib::str::string_vector;
