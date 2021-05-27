66edd73b023d6d63d55fde4bb4f2d37bf6bd1ed2
Sloan Liu <slyliu@ucsc.edu>
2021-05-27 16:40:20 -0700

    saving

--- mmap_lib/tests/bench_pstr_use.cpp
+++ mmap_lib/tests/bench_pstr_use.cpp
@@ -474,7 +474,7 @@ void bench_str_cmp() {
     Lbench b("bench_str_cmp");
 
     Lrand_range<char>     ch(33, 126);
-    Lrand_range<uint16_t> sz(1, 50);
+    Lrand_range<uint16_t> sz(1, 30);
 
     std::vector<mmap_lib::str<2>> v;
     for (auto i = 0u; i < STR_SIZE; ++i) {
@@ -502,7 +502,7 @@ void bench_str_cmp() {
     Lbench b("bench_string_cmp");
 
     Lrand_range<char>     ch(33, 126);
-    Lrand_range<uint16_t> sz(1, 50);
+    Lrand_range<uint16_t> sz(1, 30);
 
     std::vector<std::string> v;
     for (auto i = 0u; i < STR_SIZE; ++i) {
